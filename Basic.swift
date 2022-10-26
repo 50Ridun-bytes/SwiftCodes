f0adc8a03c8325acccf2c84055c21989b9f756d0
Tasrefee Mahmood Ridun <95172466+50Ridun-bytes@users.noreply.github.com>
2022-10-26 22:42:48 +0600

    Update Basic.swift

--- Basic.swift
+++ Basic.swift
@@ -1,1 +1,37 @@
+import UIKit
+
+let actor = "Tom Cruise"
+
+let quote = "He tapped a sign saying \"Believe\" and walked away."
+
+let movie = """
+A day in
+the life of
+an Apple engineer
+"""
+
+print(actor.count)
+
+print(quote.hasPrefix("He"))
+
+print(quote.hasSuffix("away."))
+
+var a = 1.1
+var b = 2.2
+var c = a + b
+
+c > 3
+c >= 3
+c > 4
+c < 4
+c
+c = 3.3
+
+var name = "I'm Ridun"
+name == "I'm RIDUN"
+name != "I'm RIDUN"
+
+var stayOutTooLate = true
+stayOutTooLate
+!stayOutTooLate
 
